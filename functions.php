233da845eb8f8becdd64f7f6af91e8101e5a8443
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-08-05 13:45:41 +0300

    Add TGM

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Plugins connect
+ */
+require get_template_directory() . '/tgm/connect.php';
+
 if ( ! class_exists( 'Timber' ) ) {
 	add_action( 'admin_notices', function() {
 		echo '<div class="error"><p>Timber not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php#timber' ) ) . '">' . esc_url( admin_url( 'plugins.php') ) . '</a></p></div>';
