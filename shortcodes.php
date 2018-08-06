a40042b3e256d6c75b3f7cde5e17b8c3d01ece7e
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-08-06 16:08:16 +0300

    Add duplicate-post plugin, change vc_new_icon

--- shortcodes.php
+++ shortcodes.php
@@ -1,30 +1,76 @@
 <?php
-	function generate_review( $atts ) {
-		// $atts = shortcode_atts( array(
-		// 	'post'   => '',
-		// 	'class'	 => 'col-12 col-sm-6'
-		// ), $atts );
-		if ( isset($atts['post']) ) {
-      $post = $atts['post'];
-    }
-    else {
-      $post = false;
-    }
+	function excerpt($id, $limit) {
+		$excerpt = explode(' ', get_the_excerpt($id), $limit);
 
-    if ( isset($atts['class']) ) {
-      $class = sanitize_text_field($atts['class']);
-    }
-    else {
-      $class = 'col-12 col-sm-6';
-    }
+		if (count($excerpt) >= $limit) {
+				array_pop($excerpt);
+				$excerpt = implode(" ", $excerpt) . '...';
+		} else {
+				$excerpt = implode(" ", $excerpt);
+		}
 
-		return Timber::compile( 
-			'partial/review.twig', 
-			array( 
-				'post' 	=> $post,
-				'class'	=> $class
-			) 
-		);
+		$excerpt = preg_replace('`\[[^\]]*\]`', '', $excerpt);
+
+		return $excerpt;
 	}
-	add_shortcode('review', 'generate_review');
+	add_shortcode('loop', 'fb_custom_query_shortcode');
+	function fb_custom_query_shortcode( $atts ) {
+		 
+		 // Defaults
+		 extract( shortcode_atts( array(
+				'posts_per_page' 	=> '2',
+				'category__in'		=> '',
+				'class'						=> 'col-12 col-sm-6',
+				'compare'					=> ''
+		 ), $atts ) );
+
+		 $lastposts = get_posts( array(
+				'posts_per_page'	=> $posts_per_page,
+				'post_type'				=> 'post',
+				'post_status'			=> 'publish',
+				'category__in'		=> $category__in,
+				'meta_query' => array(
+						array(
+							'key'     		=> 'video',
+							'value'				=> '',
+							'compare' 		=> $compare 
+						)
+				)
+		 ) );
+		 
+		 // Reset and setup variables
+		 $output = '';
+		 
+		 // the loop
+		 $output .= '<div class="row">';
+			 foreach( $lastposts as $post ){ 
+				setup_postdata($post);
+				$temp_id 				= $post->ID;
+				$temp_category 	= get_the_category($temp_id);
+				$temp_title 		= get_the_title( $temp_id );
+				$temp_link 			= get_permalink( $temp_id );
+				$temp_image 		= get_the_post_thumbnail_url($temp_id);
+				$temp_video 		= get_field('video', $temp_id);
+
+				$temp_excerpt 	= excerpt($temp_id, 20);
+
+				$output .= Timber::compile( 
+					'partial/review.twig', 
+					array( 
+						'title' 		=> $temp_title,
+						'link' 			=> $temp_link,
+						'image'			=> $temp_image,
+						'excerpt'		=> $temp_excerpt,
+						'category'	=> $temp_category[0]->cat_name,
+						'video'			=> $temp_video,
+						'class'			=> $class
+					) 
+				);	
+			 }
+		 $output .= '</div>';
+		 return $output;
+		 wp_reset_postdata();
+		 
 ?>
+
+<?php } ?>
--- visual_composer/vc-elements/vc_new_icon.php
+++ visual_composer/vc-elements/vc_new_icon.php
@@ -105,7 +105,7 @@ class vcNewIcon extends WPBakeryShortCode {
 					<div class="icon__header">
 						<img src="'.$img.'" alt="'.$title.'" class="icon__header-image">
 					</div>
-					<div class="icon__content">
+					<div class="icon__content article">
 						'.wpautop($content).'
 					</div>
 					<div class="icon__footer">
